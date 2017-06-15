35311ef487b388ec3d268bd2eaa8d4ea7a05e2b4
mengyao12 <mengyao@ualberta.ca>
2017-06-15 13:23:20 -0600

    it a test

--- ray2dmain.jl
+++ ray2dmain.jl
@@ -3,15 +3,10 @@ using ray2dmod
 parmpath = "/Users/admin/tomo2Dcode/juliaV/data/inparm.txt";
 ttpth,mdlpth,outpth,maxitr,maxV,minV,tau,hvs = readparm(parmpath);
 
+ns,iunit,mr
 
 
-
-
-
-
-
-
-
+# it a test 
 a = 4
 b = readmdl(a);
 
